c717a56243d5850d61492c641c568a386206c2e5
gabrielesasa <34100322+gabrielesasa@users.noreply.github.com>
2024-06-25 22:47:45 +0200

    Add files via upload

---
+++ src/main/java/it/uniroma3/siw/SiwModa2Application.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiwModa2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiwModa2Application.class, args);
+	}
+
+}
